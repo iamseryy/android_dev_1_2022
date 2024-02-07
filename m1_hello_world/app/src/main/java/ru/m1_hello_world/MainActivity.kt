029f3a9d2e1cc80f8450a0261797057b93eea3ec
Сергей Олегович <s_perminov@bz.ru>
2024-02-07 17:33:28 +0300

    m1_hello_world ready

--- m1_hello_world/app/src/main/java/ru/m1_hello_world/MainActivity.kt
+++ m1_hello_world/app/src/main/java/ru/m1_hello_world/MainActivity.kt
@@ -21,40 +21,53 @@ class MainActivity : AppCompatActivity() {
         binding.cancelButton.isVisible = false
 
         binding.addButton.setOnClickListener {
-            counter++
-
-            if (counter > MAX_COUNTER) {
-                binding.remainderTextView.text = resources.getString(R.string.overflow)
-                binding.remainderTextView.setTextColor(getColor(R.color.red))
-                binding.cancelButton.isVisible = true
-            }
-
-            setCommonContent(binding)
+            setContentAddButton(binding)
         }
 
         binding.reduceButton.setOnClickListener {
-            counter--
-
-            if (counter == 0) {
-                binding.reduceButton.isEnabled = false
-                binding.remainderTextView.text = resources.getString(R.string.free)
-                binding.remainderTextView.setTextColor(getColor(R.color.green))
-            }
-
-            setCommonContent(binding)
+            setContentReduceButton(binding)
         }
 
         binding.cancelButton.setOnClickListener {
-            counter = 0
-            binding.reduceButton.isEnabled = false
-            binding.cancelButton.isVisible = false
-            binding.counterTextView.text = "0"
-            binding.remainderTextView.text = resources.getString(R.string.free)
-            binding.remainderTextView.setTextColor(getColor(R.color.green))
+            setContentCancelButton(binding)
         }
     }
 
-    private fun setCommonContent(binding: ActivityMainBinding) {
+    private fun setContentAddButton(binding: ActivityMainBinding) {
+        counter++
+
+        if (counter > MAX_COUNTER) {
+            binding.remainderTextView.text = resources.getString(R.string.overflow)
+            binding.remainderTextView.setTextColor(getColor(R.color.red))
+            binding.cancelButton.isVisible = true
+        }
+
+        setCommonContentForAddReduce(binding)
+    }
+
+
+    private fun setContentReduceButton(binding: ActivityMainBinding) {
+        counter--
+
+        if (counter == 0) {
+            binding.reduceButton.isEnabled = false
+            binding.remainderTextView.text = resources.getString(R.string.free)
+            binding.remainderTextView.setTextColor(getColor(R.color.green))
+        }
+
+        setCommonContentForAddReduce(binding)
+    }
+
+    private fun setContentCancelButton(binding: ActivityMainBinding) {
+        counter = 0
+        binding.reduceButton.isEnabled = false
+        binding.cancelButton.isVisible = false
+        binding.counterTextView.text = "0"
+        binding.remainderTextView.text = resources.getString(R.string.free)
+        binding.remainderTextView.setTextColor(getColor(R.color.green))
+    }
+
+    private fun setCommonContentForAddReduce(binding: ActivityMainBinding) {
         if(counter > 0) binding.reduceButton.isEnabled = true
         if(counter in 1..< MAX_COUNTER) {
             binding.cancelButton.isVisible = false
