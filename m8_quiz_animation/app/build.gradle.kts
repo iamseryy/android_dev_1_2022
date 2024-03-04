d90597928654de5518aad5cf83f730c0e4bfe789
desktop <psomail@yandex.ru>
2024-03-04 22:53:22 +0300

    m8_quiz_animation: task2 ready

--- m8_quiz_animation/app/build.gradle.kts
+++ m8_quiz_animation/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.airbnb.android:lottie:6.3.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
