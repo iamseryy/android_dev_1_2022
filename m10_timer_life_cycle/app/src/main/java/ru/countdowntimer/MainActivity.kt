d58b19e84aa39941efa4e1e91f0789ac1baeda1a
desktop <psomail@yandex.ru>
2024-03-10 18:37:30 +0300

    task10 manage slider

--- m10_timer_life_cycle/app/src/main/java/ru/countdowntimer/MainActivity.kt
+++ m10_timer_life_cycle/app/src/main/java/ru/countdowntimer/MainActivity.kt
@@ -2,10 +2,54 @@ package ru.countdowntimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import ru.countdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var job: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setDefault()
+
+
+
+
+        binding.apply {
+            counterSlider.va
+
+            startButton.setOnClickListener {
+                if(job?.isActive == true ) {
+                    job?.cancel()
+                    startButton.text = resources.getString(R.string.start)
+                } else {
+                    startButton.text = resources.getString(R.string.stop)
+
+                    job = CoroutineScope(Dispatchers.Main).launch {
+                        (0..100).forEach{
+                            delay(1000)
+                            counterProgressBar.progress = 100 - it
+                            counterText.text = (100 - it).toString()
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun setDefault() {
+        binding.apply {
+            counterProgressBar.progress = 100
+            counterText.text = "10"
+            counterSlider.value = 10F
+        }
     }
 }
