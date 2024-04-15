05c931fee1088c12cefdf7244a6a34b4d2140da5
desktop <psomail@yandex.ru>
2024-04-15 23:35:13 +0300

    fix

--- m19_location/app/src/main/java/com/attractions/presentation/view/MainActivity.kt
+++ m19_location/app/src/main/java/com/attractions/presentation/view/MainActivity.kt
@@ -12,6 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        MapKitFactory.setApiKey("1ab58c34-c4c7-4fc9-81a4-cc68b9159109")
+        
     }
 }
